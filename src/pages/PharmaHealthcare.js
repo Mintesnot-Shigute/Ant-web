641812a415a56969a5a9de1a8988efb3f613aff7
minte775 <81554903+minte775@users.noreply.github.com>
2024-09-23 16:48:00 +0300

    Color Formats

--- src/pages/PharmaHealthcare.js
+++ src/pages/PharmaHealthcare.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
 function PharmaHealthcare() {
-
-
-
   return (
-    <div id="products"className="container mx-auto py-12">
+    <div id="products" className="container mx-auto py-12">
       <h2 className="text-5xl font-bold text-center mb-12 text-[#1B3855]">Pharmaceuticals & Healthcare Products</h2>
 
       {/* Products Section */}
       <div className="mb-12">
-      <h3 className="text-3xl font-semibold text-center mb-14 text-[#0E74A4] hover:text-gray-600 transition-colors duration-300">Our Products</h3>
+        <h3 className="text-3xl font-semibold text-center mb-14 text-[#0E74A4] hover:text-gray-600 transition-colors duration-300">Our Products</h3>
 
         <div className="flex justify-center mb-8">
           <img 
             src="/medicinefabric.jpg" 
             alt="Pharmaceuticals Products" 
             className="w-1/3 h-64 object-cover rounded-md mr-24"
+            loading="lazy"
           />
           <img 
             src="/AdobeStock_110350441.jpg" 
             alt="Pharmaceuticals Products" 
             className="w-1/3 h-64 object-cover rounded-md"
+            loading="lazy"
           />
         </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Essential Pharmaceuticals Section */}
-          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-48">
-            <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Essential Pharmaceuticals</h4>
+          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-36">
+            <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4] ">Essential Pharmaceuticals</h4>
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
@@ -76,7 +76,7 @@ function PharmaHealthcare() {
           </div>
 
           {/* Diagnostics Section */}
-          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-48">
+          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-36">
             <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Diagnostics</h4>
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
@@ -123,16 +123,19 @@ function PharmaHealthcare() {
             src="/pharma.jpg" 
             alt="Pharmaceutical Services" 
             className="w-1/3 h-64 object-cover rounded-md mr-24"
+            loading="lazy"
           />
           <img 
             src="/national-cancer-institute-aelk4Tn0vlI-unsplash.jpg" 
             alt="Pharmaceutical Services" 
             className="w-1/3 h-64 object-cover rounded-md"
+            loading="lazy"
           />
         </div>
+
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Import and Distribution Section */}
-          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-48">
+          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-36">
             <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Import and Distribution</h4>
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
@@ -156,7 +159,7 @@ function PharmaHealthcare() {
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Production of generic pharmaceuticals</span>
+                <span>Production of essential drugs</span>
               </li>
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
@@ -164,41 +167,45 @@ function PharmaHealthcare() {
               </li>
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Research and development</span>
+                <span>Research and development initiatives</span>
               </li>
             </ul>
           </div>
 
           {/* Diagnostic Services Section */}
-          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-48">
+          <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105 ml-36">
             <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Diagnostic Services</h4>
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Comprehensive diagnostic testing</span>
+                <span>Comprehensive lab testing</span>
               </li>
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Consultation with healthcare professionals</span>
+                <span>Expert consultations</span>
+              </li>
+              <li className="flex items-center">
+                <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
+                <span>Health monitoring services</span>
               </li>
             </ul>
           </div>
 
-          {/* Training Section */}
+          {/* Consultation Section */}
           <div className="bg-white rounded-lg p-6 transition-transform duration-300 hover:scale-105">
-            <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Training</h4>
+            <h4 className="text-2xl font-semibold mb-4 text-[#0E74A4]">Consultation</h4>
             <ul className="list-disc list-inside space-y-2 mb-4 text-gray-600 pl-6">
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Workshops for healthcare professionals</span>
+                <span>Healthcare system navigation</span>
               </li>
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Training on equipment usage and safety</span>
+                <span>Personalized health strategies</span>
               </li>
               <li className="flex items-center">
                 <span className="w-3 h-3 bg-[#0E74A4] rounded-full mr-2"></span>
-                <span>Continuing education programs</span>
+                <span>Medication management</span>
               </li>
             </ul>
           </div>
